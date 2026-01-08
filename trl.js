eed36cb8846d882482a7e8f8ac56e60724f3d42f
luisandresdelgadomatta-crypto <luisandresdelgadomatta@gmail.com>
2026-01-08 15:34:17 +0100

    Add files via upload

---
+++ trl.js
@@ -0,0 +1,2 @@
+// trl.js
+const webhookUrl = 'https://discord.com/api/webhooks/1457175826503045190/d-ACZsJnPJlMKHoywPgE1rqN4nUoJgNslaQHSix8TWmK_-WVQESToY_1m43EyjpPvGJE'; // Reemplaza esto con tu Webhook real
